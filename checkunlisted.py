da2822683af756da5a32d875e97b52fa28403444
britmob <maxmob04@gmail.com>
2021-06-25 14:00:06 -0400

    add unlisted checker

---
+++ checkunlisted.py
@@ -0,0 +1,21 @@
+import requests
+import re
+
+num = 0
+numu = 0
+
+with open('yt-speedrun-links.txt', 'r') as f:
+    urls = f.read()
+
+urls = urls.split('\n')
+
+for url in urls:
+    time.sleep(0.5)
+    num +=1
+    print(f"Checked {num} urls - {url}. {numu} unlisted so far.")
+    data = requests.get(url).text
+    if 'ytd-badge-supported-renderer">Unlisted' in data:
+        with open('yt-speedrun-links-unlisted.txt', 'a') as f:
+            f.write(url + "\n")
+            numu += 1
+            
--- getruns.py
+++ getruns.py
@@ -10,25 +10,33 @@ os.chdir(sys.path[0])
 while True:
     time.sleep(0.6)
     url = f"https://www.speedrun.com/api/v1/runs?max=200&offset={num}"
-
+    print(url)
 
     data = requests.get(url).text
     data = json.loads(data)
 
     for run in data['data']:
+        years = ['2021', '2020', '2019', '2018']
+        
+
+
         try:
+            date = run['date']
+
+            if any(x in date for x in years):
+                pass
+
             for video in run['videos']['links']:
                 video = video['uri']
                 if not 'you' in video:
                     pass
                 print(video)
                 with open('yt-speedrun-links.txt', 'a') as f:
-                    f.write(video)
+                    f.write(video + "\n")
 
         except:
             pass
     num += 200
-    print(num)
 
 
 
