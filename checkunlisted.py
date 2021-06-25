2880408a36ef61f62d41f844efb0e9474771eec7
britmob <maxmob04@gmail.com>
2021-06-25 14:12:32 -0400

    edit check script a bit

--- checkunlisted.py
+++ checkunlisted.py
@@ -10,11 +10,17 @@ with open('yt-speedrun-links.txt', 'r') as f:
 
 urls = urls.split('\n')
 
+
 for url in urls:
+    url = f'https://www.youtube.com/watch?v={url}'
     time.sleep(0.5)
     num +=1
     print(f"Checked {num} urls - {url}. {numu} unlisted so far.")
-    data = requests.get(url).text
+    try:
+        data = requests.get(url).text
+    except:
+        print(f'{url} - FAILED')
+        pass
     if 'ytd-badge-supported-renderer">Unlisted' in data:
         with open('yt-speedrun-links-unlisted.txt', 'a') as f:
             f.write(url + "\n")
